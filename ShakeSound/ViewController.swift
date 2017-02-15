81ac45f8fcdefd4026007d407bf36f790aa8ccc5
Watcha Pon <watchapon@watchas-mac.local>
2017-02-15 08:36:04 -0800

    Initial

--- ShakeSound/ViewController.swift
+++ ShakeSound/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer = AVAudioPlayer()
+    var soundFiles = ["boing","explosion","hit","knife","shoot","swish","wah","warble"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+	
+    func randomSound(){
+//        guard let path = Bundle.main.url(forResource: "hit", withExtension: "mp3") else {
+        let randomSound = Int(arc4random_uniform(UInt32(soundFiles.count)))
+        guard let path = NSDataAsset(name: soundFiles[randomSound]) else {
+            print("Path not Found")
+            return
+        }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+//            audioPlayer = try AVAudioPlayer(contentsOf: path, fileTypeHint: AVFileTypeMPEGLayer3 )
+            audioPlayer = try AVAudioPlayer(data: path.data, fileTypeHint: AVFileTypeMPEGLayer3)
+            audioPlayer.play()
+        } catch let error as NSError{
+        print("error : \(error.localizedDescription)")
+        }
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake{
+            randomSound()
+        }
+    }
 
 }
 
